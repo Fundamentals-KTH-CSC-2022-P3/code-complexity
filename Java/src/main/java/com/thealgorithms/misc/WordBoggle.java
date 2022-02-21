c128614932d6f304343c5e87007421b0f4ec35a2
Arvid Siberov <asiberov@kth.se>
2022-02-21 13:30:21 +0100

    Add comment with testing requirements to WordBoggle::getNeighbors

--- Java/src/main/java/com/thealgorithms/misc/WordBoggle.java
+++ Java/src/main/java/com/thealgorithms/misc/WordBoggle.java
@@ -83,6 +83,19 @@ public class WordBoggle {
         visited[i][j] = false;
     }
 
+    /**
+     * This method lacked tests entirely.
+     *
+     * Returns all valid neighbor coordinates in a 2d array. Invalid coordinates are exactly the same as
+     * out of bounds coordinates. Neighbors are coordinates on the form (i ± 1, j ± 1).
+     *
+     * This means that "corner" coordinates should result in three neighbors, "edge" coordinates five neighbors, and
+     * "inner" coordinates eight neighbors.
+     *
+     * N.B. I *think* that you are only ever meant to call this method with 0 ≤ i < board.length and 0 ≤ j < board[0].length
+     * and board[n].length = board[m].length for all n, m. However, no bounds checking is done in the original
+     * implementation, and no documentation was written, so this is guesswork from me. //Arvid
+     */
     public static List<Integer[]> getNeighbors(int i, int j, char[][] board) {
         List<Integer[]> neighbors = new ArrayList<>();
         if (i > 0 && j > 0) {
