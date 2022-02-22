7c4251e22093af8d4fe79c954ad4a1ed6e458295
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-02-22 18:17:15 +0100

    update the test suite

--- Java/src/test/java/com/thealgorithms/datastructures/BinaryTreeTest.java
+++ Java/src/test/java/com/thealgorithms/datastructures/BinaryTreeTest.java
@@ -1,4 +1,10 @@
 package com.thealgorithms.datastructures;
 
+import com.thealgorithms.datastructures.trees.BinaryTree;
+import org.junit.jupiter.api.AfterAll;
+
 public class BinaryTreeTest {
+
+    
+
 }
