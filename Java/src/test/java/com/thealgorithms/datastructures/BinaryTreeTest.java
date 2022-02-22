93d67d02c19cfecd92dd748db34a4c57e3c37a27
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-02-22 12:48:20 +0100

    add empty test class with tear down

---
+++ Java/src/test/java/com/thealgorithms/datastructures/BinaryTreeTest.java
@@ -0,0 +1,20 @@
+package com.thealgorithms.datastructures;
+
+import com.thealgorithms.datastructures.trees.BinaryTree;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BinaryTreeTest {
+    @AfterAll
+    static void tearDown() {
+        for (int i = 0; i < BinaryTree.numBranchesInRemove; i++) {
+            if (!BinaryTree.coveredBranches.contains(i)) {
+                System.out.println("Branch " + i + " not covered in WordBoggle::getNeighbors");
+            }
+        }
+        double coverage = BinaryTree.coveredBranches.size() / (double) BinaryTree.numBranchesInRemove;
+        System.out.printf("Total branch coverage of BinaryTree::remove: %.2f%%\n", coverage*100);
+    }
+}
