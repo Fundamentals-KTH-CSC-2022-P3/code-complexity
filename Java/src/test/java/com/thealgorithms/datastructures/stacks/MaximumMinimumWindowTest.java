55ba41f3e105a2a324699ca6439afaa106238ec0
katri <katrina.liang5@gmail.com>
2022-02-23 22:23:25 +0100

    Add two tests for 100% brnach coverage of MaximumMinimumWindow::calculateMaxOfMin

---
+++ Java/src/test/java/com/thealgorithms/datastructures/stacks/MaximumMinimumWindowTest.java
@@ -0,0 +1,35 @@
+package com.thealgorithms.datastructures.stacks;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The function MaximumMinimumWindow::calculateMaxOfMin did not have any test suite and the
+ * branch coverage was 0%.
+ *
+ * Notice that the function only takes array of positive integers. The only requirement that
+ * needs to be tested is that the input array consists of positive integers. This will give a
+ * branch coverage of 100%.
+ */
+public class MaximumMinimumWindowTest {
+    /**
+     * Test the requirement to input an array of positive integers
+     */
+    @Test
+    void successTest1(){
+        int[] arr = new int[]{10, 20, 30, 10};
+        int[] target = new int[]{30, 20, 10, 10};
+        int[] res = MaximumMinimumWindow.calculateMaxOfMin(arr, arr.length);
+        assertArrayEquals(target, res);
+    }
+
+    /**
+     * Test the requirement to input an array of positive integers of length 1
+     */
+    @Test
+    void successTest2(){
+        int[] arr = new int[]{0};
+        int[] target = new int[]{0};
+        int[] res = MaximumMinimumWindow.calculateMaxOfMin(arr, arr.length);
+        assertArrayEquals(target, res);
+    }
+}
