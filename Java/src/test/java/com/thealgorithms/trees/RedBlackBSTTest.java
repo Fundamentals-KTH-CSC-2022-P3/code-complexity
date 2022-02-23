547ed7f5a371dbfee4a6002f654a3f330ba94f84
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2022-02-23 22:04:36 +0100

    add some more tests

--- Java/src/test/java/com/thealgorithms/trees/RedBlackBSTTest.java
+++ Java/src/test/java/com/thealgorithms/trees/RedBlackBSTTest.java
@@ -19,7 +19,7 @@ public class RedBlackBSTTest {
     }
 
     @Test
-    void testTreeCanDeleteNode() {
+    void testTreeCanDeleteBlackNode() {
         RedBlackBST tree = new RedBlackBST();
 
         tree.insert(tree.createNewNode(1));
@@ -28,8 +28,70 @@ public class RedBlackBSTTest {
         tree.insert(tree.createNewNode(4));
         tree.insert(tree.createNewNode(5));
 
+        // the node "1" here, would be black
         assertNotNull(tree.findNode(tree.createNewNode(1), tree.root));
         tree.delete(tree.createNewNode(1));
         assertNull(tree.findNode(tree.createNewNode(1), tree.root));
+    }
+
+    @Test
+    void testTreeCanDeleteRedNode() {
+        RedBlackBST tree = new RedBlackBST();
+
+        tree.insert(tree.createNewNode(1));
+        tree.insert(tree.createNewNode(2));
+        tree.insert(tree.createNewNode(3));
+        tree.insert(tree.createNewNode(4));
+        tree.insert(tree.createNewNode(5));
+
+        // the node "5" here, would be red
+        assertNotNull(tree.findNode(tree.createNewNode(5), tree.root));
+        tree.delete(tree.createNewNode(5));
+        assertNull(tree.findNode(tree.createNewNode(5), tree.root));
+    }
+
+    @Test
+    void testTreeCanDeleteAllNodesInATree() {
+        RedBlackBST tree = new RedBlackBST();
+
+        tree.insert(tree.createNewNode(1));
+        tree.insert(tree.createNewNode(2));
+        tree.insert(tree.createNewNode(3));
+        tree.insert(tree.createNewNode(4));
+        tree.insert(tree.createNewNode(5));
+
+        tree.delete(tree.createNewNode(1));
+        tree.delete(tree.createNewNode(2));
+        tree.delete(tree.createNewNode(3));
+        tree.delete(tree.createNewNode(4));
+        tree.delete(tree.createNewNode(5));
+        assertNull(tree.findNode(tree.createNewNode(1), tree.root));
+        assertNull(tree.findNode(tree.createNewNode(2), tree.root));
+        assertNull(tree.findNode(tree.createNewNode(3), tree.root));
+        assertNull(tree.findNode(tree.createNewNode(4), tree.root));
+        assertNull(tree.findNode(tree.createNewNode(5), tree.root));
+    }
+
+    @Test
+    void testTreeCanDeleteTheRootNode() {
+        RedBlackBST tree = new RedBlackBST();
+
+        tree.insert(tree.createNewNode(1));
+        tree.insert(tree.createNewNode(2));
+        tree.insert(tree.createNewNode(3));
+        tree.insert(tree.createNewNode(4));
+        tree.insert(tree.createNewNode(5));
+
+        assertEquals(2, tree.root.key);
+        tree.delete(tree.createNewNode(2));
+
+        assertNotEquals(2, tree.root.key);
+        assertNull(tree.findNode(tree.createNewNode(2), tree.root));
+
+        // All other nodes should be there
+        assertNotNull(tree.findNode(tree.createNewNode(1), tree.root));
+        assertNotNull(tree.findNode(tree.createNewNode(3), tree.root));
+        assertNotNull(tree.findNode(tree.createNewNode(4), tree.root));
+        assertNotNull(tree.findNode(tree.createNewNode(5), tree.root));
     }
 }
